e08e7737b2bc33104bb15134bb7b257faafcbe55
Kai Kent <kkent17@stanford.edu>
2016-12-07 17:58:05 -0800

    Copied some code from WGCNA tutorial; detected modules

--- construct_network.R
+++ construct_network.R
@@ -93,10 +93,106 @@ runPickSoftThreshold = function(
 	return(output$fitIndices);
 }
 
+# This function copied from https://labs.genetics.ucla.edu/horvath/CoexpressionNetwork/Rpackages/WGCNA/Tutorials/FemaleLiver-02-networkConstr-auto.pdf
+plotThresholdResults = function(
+	rpst_output # returned by runPickSoftThreshold
+) {
+	par(mfrow = c(1,2));
+	powers = rpst_output$Power;
+	cex1 = 0.9;
+	sft = list(fitIndices = rpst_output);
+
+	plot(
+		sft$fitIndices[,1],
+		-sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+		xlab="Soft Threshold (power)",
+		ylab="Scale Free Topology Model Fit,signed R^2",
+		type="n",
+		main = paste("Scale independence")
+	);
+	text(
+		sft$fitIndices[,1],
+		-sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+		labels=powers,
+		cex=cex1,
+		col="red"
+	);
+
+	plot(
+		sft$fitIndices[,1],
+		sft$fitIndices[,5],
+		xlab="Soft Threshold (power)",
+		ylab="Mean Connectivity",
+		type="n",
+		main = paste("Mean connectivity")
+	);
+	text(
+		sft$fitIndices[,1],
+		sft$fitIndices[,5],
+		labels=powers,
+		cex=cex1,
+		col="red"
+	);
+}
+
+
+constructNetworkAndModules = function(
+	trans_and_rg_data,
+	beta
+) {
+	net = blockwiseModules(
+		t(trans_and_rg_data$transcription_data),
+		power = beta,
+		TOMType = "unsigned", # TOM = Topological Overlap Matrix
+		minModuleSize = 30,
+		reassignThreshold = 0, # reassign genes that are closer to another cluster
+		mergeCutHeight = 0.25,
+		numericLabels = TRUE,
+		pamRespectsDendro = FALSE,
+		saveTOMs = FALSE,
+		verbose = 3
+	);
+	return(net);
+}
+
+
+plotBasicDendrogram = function(
+	constructNetworkAndModules_output
+) {
+	net = constructNetworkAndModules_output;
+	#sizeGrWindow(12, 9);
+	mergedColors = labels2colors(net$colors);
+	plotDendroAndColors(
+		net$dendrograms[[1]],
+		mergedColors[net$blockGenes[[1]]],
+		"Module colors",
+		dendroLabels = FALSE,
+		hang = 0.03,
+		addGuide = TRUE,
+		guideHang = 0.05
+	);
+}
+
+
+save_module_data = function (
+	constructNetworkAndModules_output,
+	outfile = 'network_modules'
+) {
+	net = constructNetworkAndModules_output;
+	moduleLabels = net$colors;
+	moduleColors = labels2colors(net$colors);
+	MEs = net$MEs;
+	geneTree = net$dendrograms[[1]];
+	save(MEs, moduleLabels, moduleColors, geneTree,
+		file = paste0(outfile, ".RData")
+	)
+}
+
+
 # Target:
 # r^2 > 0.8 (at least 0.7)
 # slope between -1 and -2
-# mean connectivity between 15 and 20
+# mean connectivity at least 15 to 20
 # lower is better
 
 
